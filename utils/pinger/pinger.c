c2a7796c10f4bde770db8d3d1c3ccb3859e13802
UBA-code <belhachmiyassine4@gmail.com>
2025-04-25 20:45:42 +0100

    FIX: fix the 100% cpu usage cased by the infinite loop with a 5ms sleep between each iteration

--- utils/pinger/pinger.c
+++ utils/pinger/pinger.c
@@ -6,7 +6,7 @@
 /*   By: ybel-hac <ybel-hac@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/13 00:31:15 by ybel-hac          #+#    #+#             */
-/*   Updated: 2025/04/25 15:05:59 by ybel-hac         ###   ########.fr       */
+/*   Updated: 2025/04/25 20:43:20 by ybel-hac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -110,6 +110,8 @@ void pinger(char *host, bool once)
         progressInvalidReply(
             recvBuffer, bytesReceived, ip_str, ipHeader);
     }
+    // sleep for 5ms
+    usleep(5 * 1000);
   }
   freeaddrinfo(ping_struct->results);
   ping_struct->results = NULL;
