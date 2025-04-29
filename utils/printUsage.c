a528dfe9169d67d1dd2b6c77250f85e371b47c82
UBA-code <belhachmiyassine4@gmail.com>
2025-04-29 07:52:23 +0100

    FIX: little miss in the usage

--- utils/printUsage.c
+++ utils/printUsage.c
@@ -6,7 +6,7 @@
 /*   By: ybel-hac <ybel-hac@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 17:48:27 by ybel-hac          #+#    #+#             */
-/*   Updated: 2025/04/20 19:54:25 by ybel-hac         ###   ########.fr       */
+/*   Updated: 2025/04/29 07:52:07 by ybel-hac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void printUsage()
 	printf("-c\tstop after sending <count> packets\n");
 	printf("-d\tset the SO_DEBUG option\n");
 	printf("-v\tverbose output\n");
-	printf("-w\tstop after N seconds (not implemented yet)\n");
+	printf("-w\tstop after N seconds\n");
 	printf("-W\tnumber of seconds to wait for response\n");
 	printf("-q\tquiet output\n");
 	printf("\n");
